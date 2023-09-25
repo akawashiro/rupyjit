c450eb0f429613ad7e5dedd0bea3127d7ed753b6
Akira Kawata <akirakawata1@gmail.com>
2023-09-25 19:46:07 +0900

    Show disasm

--- src/pyutils.rs
+++ src/pyutils.rs
@@ -9,14 +9,16 @@ use pyo3::ffi::{
 };
 use std::ffi::CStr;
 use std::io::{self, Write};
+use std::str::FromStr;
 
 #[path = "bytecode.rs"]
 mod bytecode;
 use bytecode::Bytecode;
 
 extern crate libc;
+use iced_x86::{Decoder, DecoderOptions, Formatter, Instruction, NasmFormatter};
 use libc::{c_int, c_void, size_t, PROT_EXEC, PROT_READ, PROT_WRITE};
-use std::alloc::{alloc, dealloc, Layout};
+use std::alloc::{alloc, Layout};
 
 extern "C" {
     fn mprotect(addr: *const c_void, len: size_t, prot: c_int) -> c_int;
@@ -24,10 +26,10 @@ extern "C" {
 
 macro_rules! jit_log {
     ($x:expr) => {
-        io::stdout().write_all(
+        let _ = io::stdout().write_all(
             (file!().to_owned() + ":" + &line!().to_string() + " jit_log " + $x + "\n").as_bytes(),
         );
-        io::stdout().flush();
+        let _ = io::stdout().flush();
     };
 }
 
@@ -343,6 +345,11 @@ pub fn compile_and_exec_jit_code(
             }
             assert_eq!(offset - start_offset, bytes_per_code.try_into().unwrap());
         }
+        if std::env::var("RUST_LOG") == Result::Ok(String::from("info"))
+            || std::env::var("RUST_LOG") == Result::Ok(String::from("debug"))
+        {
+            log_disasm(p_start, offset);
+        }
     }
     let code: fn() -> *mut PyObject = unsafe { std::mem::transmute(p_start) };
 
@@ -350,6 +357,63 @@ pub fn compile_and_exec_jit_code(
     let retval = code();
     info!("Return from code:{:x?} retval:{:x?}", code, retval);
     return Some(retval);
+}
+
+fn log_disasm(code: *const u8, code_size: usize) {
+    let mut code_vec: Vec<u8> = Vec::new();
+    for i in 0..code_size {
+        code_vec.push(unsafe { *code.offset(i as isize) });
+    }
+    let bytes = code_vec.as_slice();
+    let bitness = 64;
+    let rip = code as u64;
+    const HEXBYTES_COLUMN_BYTE_LENGTH: usize = 10;
+    let mut decoder = Decoder::with_ip(bitness, code_vec.as_slice(), rip, DecoderOptions::NONE);
+
+    // Formatters: Masm*, Nasm*, Gas* (AT&T) and Intel* (XED).
+    // For fastest code, see `SpecializedFormatter` which is ~3.3x faster. Use it if formatting
+    // speed is more important than being able to re-assemble formatted instructions.
+    let mut formatter = NasmFormatter::new();
+
+    // Change some options, there are many more
+    formatter.options_mut().set_digit_separator("`");
+    formatter.options_mut().set_first_operand_char_index(10);
+
+    // String implements FormatterOutput
+    let mut output = String::new();
+
+    // Initialize this outside the loop because decode_out() writes to every field
+    let mut instruction = Instruction::default();
+
+    // The decoder also implements Iterator/IntoIterator so you could use a for loop:
+    //      for instruction in &mut decoder { /* ... */ }
+    // or collect():
+    //      let instructions: Vec<_> = decoder.into_iter().collect();
+    // but can_decode()/decode_out() is a little faster:
+    while decoder.can_decode() {
+        // There's also a decode() method that returns an instruction but that also
+        // means it copies an instruction (40 bytes):
+        //     instruction = decoder.decode();
+        decoder.decode_out(&mut instruction);
+
+        // Format the instruction ("disassemble" it)
+        output.clear();
+        formatter.format(&instruction, &mut output);
+
+        // Eg. "00007FFAC46ACDB2 488DAC2400FFFFFF     lea       rbp,[rsp-100h]"
+        print!("{:016X} ", instruction.ip());
+        let start_index = (instruction.ip() - rip) as usize;
+        let instr_bytes = &bytes[start_index..start_index + instruction.len()];
+        for b in instr_bytes.iter() {
+            print!("{:02X}", b);
+        }
+        if instr_bytes.len() < HEXBYTES_COLUMN_BYTE_LENGTH {
+            for _ in 0..HEXBYTES_COLUMN_BYTE_LENGTH - instr_bytes.len() {
+                print!("  ");
+            }
+        }
+        println!(" {}", output);
+    }
 }
 
 pub fn show_code_vec(code_vec: &Vec<u8>) {
